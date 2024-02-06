8f01749ff30af860b1f3b681db49a3080b628e0b
Nour El Din Shady <rhasoldy@gmail.com>
2024-02-07 01:59:02 +0200

    feat(navbar): created desktop navbar

---
+++ src/components/widgets/Navbar/index.tsx
@@ -0,0 +1,40 @@
+import CV from "../../icons/cv.svg";
+import Github from "../../icons/github.svg";
+import Linkedin from "../../icons/linkedin.svg";
+import RhasoldyLogo from "../../icons/rhasoldy.svg";
+
+const Navbar = () => {
+  const links = [
+    { href: "/#Work", label: "Work" },
+    { href: "/#Skills", label: "Skills" },
+    { href: "/#Blog", label: "Blog" },
+  ];
+
+  return (
+    <nav className="px-xl flex items-center justify-between py-4">
+      <a href="/" aria-label="home link">
+        <img src={RhasoldyLogo.src} alt="logo" />
+      </a>
+      <ul className="flex items-center gap-12">
+        {links.map(link => (
+          <a key={link.href} href={link.href}>
+            {link.label}
+          </a>
+        ))}
+        <ul className="flex gap-4">
+          <a href="/" aria-label="github link">
+            <img src={Github.src} alt="github logo" />
+          </a>
+          <a href="/" aria-label="linkedin link">
+            <img src={Linkedin.src} alt="linkedin logo" />
+          </a>
+          <a href="/" aria-label="cv link">
+            <img src={CV.src} alt="cv logo" />
+          </a>
+        </ul>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#101010",
+        dominant: "#FDFDFD",
+        primary: {
+          light: "#F5F5F5",
+          DEFAULT: "#FF4FA3",
+          dark: "#F5F5F5",
+        },
+        secondary: {
+          light: "#F5F5F5",
+          DEFAULT: "#FFCF70",
+          dark: "#F5F5F5",
+        },
+      },
+      padding: {
+        xl: "11rem",
+      },
+    },
   },
   plugins: [],
 };
