466934fd5fc7836def5be3ba2eb0e3c72004ada9
Nour El Din Shady <rhasoldy@gmail.com>
2024-02-08 01:33:25 +0200

    feat(quote): quote is now animated

--- src/components/widgets/Quote/Blinker.tsx
+++ src/components/widgets/Quote/Blinker.tsx
@@ -2,13 +2,14 @@ import { type Variants, motion } from "framer-motion";
 
 const blinkerVariants: Variants = {
   blinking: {
-    opacity: [0, 0, 1, 1],
+    opacity: [1, 1, 0, 0],
     transition: {
       duration: 1,
       repeat: Infinity,
       repeatDelay: 0,
-      ease: "linear",
+      ease: "easeInOut",
       times: [0, 0.5, 0.5, 1],
+      delay: 3,
     },
   },
 };
@@ -18,7 +19,7 @@ const Blinker = () => {
     <motion.div
       animate="blinking"
       variants={blinkerVariants}
-      className="bg-accent inline-block h-5 w-[3px] translate-y-1"
+      className="bg-accent ml-1 inline-block h-5 w-[2px] translate-y-1 md:w-[3px]"
     />
   );
 };
--- src/components/widgets/Quote/index.tsx
+++ src/components/widgets/Quote/index.tsx
@@ -1,22 +1,40 @@
 import clsx from "clsx";
-import type { ReactNode } from "react";
+import {
+  animate,
+  motion,
+  useInView,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
+import { useEffect, useRef } from "react";
 
 import Blinker from "./Blinker";
 
-const Quote = ({
-  className,
-  children,
-}: {
-  className?: string;
-  children: ReactNode;
-}) => {
+const Quote = ({ className, quote }: { className?: string; quote: string }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, latest => Math.round(latest));
+
+  const displayText = useTransform(rounded, (latest: number) =>
+    quote.slice(0, latest),
+  );
+
+  useEffect(() => {
+    animate(count, quote.length, {
+      type: "tween",
+      duration: 3,
+      ease: "easeOut",
+      autoplay: isInView,
+    });
+  }, [count, quote.length, isInView]);
+
   return (
-    <p className={clsx("px-9vw font-serif md:text-xl", className)}>
+    <p ref={ref} className={clsx("px-9vw font-serif md:text-xl", className)}>
       <span className="text-primary-dark block">{"/**"}</span>
-      <span>
-        {children}
-        <Blinker />
-      </span>
+      <motion.span>{displayText}</motion.span>
+      <Blinker />
       <span className="text-primary-dark block">{"*/"}</span>
     </p>
   );
