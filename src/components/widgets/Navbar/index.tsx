71f09fcd06e017a108bb0529e38ce9a769083c33
Nour El Din Shady <rhasoldy@gmail.com>
2024-02-07 19:59:31 +0200

    refactor(hero header): refactored hero header into its own component and returned back to original structure

--- src/components/widgets/Navbar/index.tsx
+++ src/components/widgets/Navbar/index.tsx
@@ -1,9 +1,9 @@
 import { useMediaQuery } from "@uidotdev/usehooks";
 
-import CV from "../icons/cv.svg";
-import Github from "../icons/github.svg";
-import Linkedin from "../icons/linkedin.svg";
-import RhasoldyLogo from "../icons/rhasoldy.svg";
+import CV from "../../icons/cv.svg";
+import Github from "../../icons/github.svg";
+import Linkedin from "../../icons/linkedin.svg";
+import RhasoldyLogo from "../../icons/rhasoldy.svg";
 
 const Navbar = () => {
   const links = [
