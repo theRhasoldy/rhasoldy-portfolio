c1291a63b7cdd82e4bdd111e23cf08b955dbf17c
Nour El Din Shady <rhasoldy@gmail.com>
2024-02-07 23:04:33 +0200

    chore: fixed react error in astro

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -66,6 +66,7 @@ module.exports = {
         // override/add rules settings here, such as:
         // "astro/no-set-html-directive": "error"
         "react/no-unknown-property": "off",
+        "react/jsx-no-undef": "off",
       },
     },
   ],
